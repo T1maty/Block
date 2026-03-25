a92f86b80e2a50a8b2cbf90a7d9bccc615461f88
T1maty <ttitaron@gmail.com>
2026-03-25 20:19:51 +0100

    Fix issue with docker compose, and finally running the nginx

--- backend/database.py
+++ backend/database.py
@@ -1,12 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
+from dotenv import load_dotenv
+
+
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), ".env"))
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-if not DATABASE_URL:
-    raise ValueError("DATABASE_URL is not set!")
-
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL)    
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
